599f7373653e3f9dd6563c3d8ff60dbb4f82af7f
SparkWanted <igq.ve1.yqw@live.com.pt>
2012-10-19 01:40:18 -0700

    problema1v1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Problema1::Application.config.secret_token = 'c0b79459b143c729396fa9b3332a46d62cb9f5c30747ae158dda05c02107c7d9ded2bafac71031b28ebc9febd38fc95556e9097f94645537f6e6951cb464481c'
